cd33190fe90f6dc2d81b01f4937038ccc60fb525
Darshan KM <darshan.km13@gmail.com>
2020-10-03 23:47:37 +0530

    Update todo
    Now to do object can be update, reset.

--- src/app/to-do/to-do.ts
+++ src/app/to-do/to-do.ts
@@ -1,85 +1,76 @@
 import { element } from 'protractor';
 export class ToDo {
-  public id: number;
-  public title: string;
-  public desc: string;
-  public startDate: string;
-  public endDate: string;
+  title:string;
+  desc:string;
+  start:string;
+  end:string;
+
   public toDoList = [
     {
-      id: 1,
       title: 'my to do 1',
       desc: 'Create an to do app 1',
       start: '29-sep-2020',
       end: '30-sep-2020',
     },
     {
-      id: 2,
       title: 'my to do 2',
       desc: 'Create an to do app 2',
       start: '30-sep-2020',
       end: '1-oct-2020',
     },
     {
-      id: 3,
       title: 'my to do 3',
       desc: 'Create an to do app 3',
       start: '1-oct-2020',
       end: '2-oct-2020',
     },
   ];
-  public add() {
+
+  // Add To do to list "toDoList"
+  public add(title:string, desc:string, start:string, end:string ) {
     this.toDoList.push({
-        id: 4,
-        title: 'my to do 4',
-        desc: 'Create an to do app 4',
-        start: '1-oct-2020',
-        end: '2-oct-2020',
+        title: title,
+        desc: desc,
+        start: start,
+        end: end
       });
+    this.emptyInputFields();  
     this.display();
+    
   }
 
-  public update(id, element) {
-    this.toDoList.filter((ele) => {
-      if (ele.id === id) {
-        this.toDoList.splice(this.toDoList.indexOf(id), 1, element);
+  // Update new data to  "To do to list".
+  public update(id) {
+    this.toDoList.filter((ele, index)=>{
+      if(id===index){
+      this.title = ele.title;
+      this.desc = ele.desc;
+      this.start=ele.start;
+      this.end=ele.end;
+      this.toDoList.splice(id,1);
       }
-    });
-    this.display();
+      this.display();
+    })
   }
 
+  // Delete object from to do list 
   public delete(id) {
-    this.toDoList.splice(this.toDoList.indexOf(id), 1);
+    this.toDoList.splice(id, 1);
     this.display();
   }
 
+
+  // Display array
   public display() {
     return this.toDoList;
   }
 
-  public reset(){
-      this.toDoList = [
-        {
-          id: 1,
-          title: 'my to do 1',
-          desc: 'Create an to do app 1',
-          start: '29-sep-2020',
-          end: '30-sep-2020',
-        },
-        {
-          id: 2,
-          title: 'my to do 2',
-          desc: 'Create an to do app 2',
-          start: '30-sep-2020',
-          end: '1-oct-2020',
-        },
-        {
-          id: 3,
-          title: 'my to do 3',
-          desc: 'Create an to do app 3',
-          start: '1-oct-2020',
-          end: '2-oct-2020',
-        },
-      ]
+  
+  // Reset all input value to empty 
+  emptyInputFields(){
+    this.title = '';
+      this.desc = '';
+      this.start= '';
+      this.end= '';
   }
 }
